cd3445a29d394b9f945afe4fc9d01f92998ebf4e
Jeffrey Schang <schangj09@gmail.com>
2023-12-18 19:19:00 +0100

    permutations - 18 Dec 2023

---
+++ src/main/java/leetcode/permutations/Solution.java
@@ -0,0 +1,39 @@
+package leetcode.permutations;
+
+import java.util.ArrayList;
+import java.util.List;
+/*
+ Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+ */
+public class Solution {
+    
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(nums[i]);
+        }
+        return permute(list);
+    }
+    
+    List<List<Integer>> permute(List<Integer> list) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (list.isEmpty()) {
+            res.add(new ArrayList<>());
+        } else {
+            // choose one of nums, remove it from the list, get permutations for the rest of the list
+            int n = list.size();
+            for (int i = 0; i < n; i++) {
+                int v = list.remove(i);
+                List<List<Integer>> perms = permute(list);
+                list.add(i, v);
+
+                // put the permutations into result
+                for (List<Integer> l : perms) {
+                    l.add(0, v);
+                    res.add(l);
+                }
+            }
+        }
+        return res;
+    }
+}
