24ba4ca2c9f6d85259d87945a688cc31083766d7
Jeffrey Schang <schangj09@gmail.com>
2023-12-18 12:40:34 +0100

    Serialize Deserialize a tree Get all substrings (or all sub arrays of an array) 18 Dec 2023

---
+++ src/main/java/leetcode/getSubstrings/Solution.java
@@ -0,0 +1,36 @@
+package leetcode.getSubstrings;
+
+import java.util.*;
+
+public class Solution {
+    // get all pssible substrings of a string - in order of starting point in the string
+    List<String> getSubStrings(String s) {
+        int n = s.length();
+        List<String> res = new ArrayList<>();
+        for (int start = 0; start < n; start++) {
+            for (int size = 1; size < n - start + 1; size++) {
+                String sub = s.substring(start, start + size);
+                res.add(sub);
+            }
+        }
+        //res.sort((a,b) -> {return a.length() - b.length();});
+        return res;
+    }
+    // get all pssible substrings of a string - in order of size (easier to read output)
+    List<String> getSubStrings2(String s) {
+        int n = s.length();
+        List<String> res = new ArrayList<>();
+        for (int size = 1; size <= n; size++) {
+            for (int j = 0; j+size <= n; j++) {
+                String sub = s.substring(j, j+size);
+                res.add(sub);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().getSubStrings("123"));
+        System.out.println(new Solution().getSubStrings("890123"));
+    }
+}
---
+++ src/main/java/leetcode/serDeserTree/Codec.java
@@ -0,0 +1,136 @@
+package leetcode.serDeserTree;
+
+public class Codec {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /*
+     * test cases
+     * 
+     * single node: 5
+     * right only: 5(,10)
+     * left only: 5(2,)
+     * negative cases: -5(-3,-200)
+     * subtree cases: 5((3,),(200(,300)))
+     * 
+     * fail: 1(2,3(4,5))
+     */
+
+    // Strategy:
+    // use a codec like this: val(<left>,<right>) where left and right are recursive
+    // and can be empty, parens can be omitted for leaf node
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuffer sb = new StringBuffer();
+        serialize(root, sb);
+        return sb.toString();
+    }
+
+    public void serialize(TreeNode node, StringBuffer sb) {
+        if (node == null) {
+            return;
+        }
+        sb.append(node.val);
+        if (node.left != null || node.right != null) {
+            sb.append("(");
+            serialize(node.left, sb);
+            sb.append(",");
+            serialize(node.right, sb);
+            sb.append(")");
+        }
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        TreeNode root = new Deser(0).exec(data);
+        // throw exception if Deser.i is not == data.length()
+        return root;
+    }
+
+    static class Deser {
+        int i;
+
+        Deser(int i) {
+            this.i = i;
+        }
+
+        public TreeNode exec(String data) {
+            int n = data.length();
+            if (i == n) {
+                return null;
+            }
+            char next = data.charAt(i);
+            // empty left or empty right node
+            if (next == ',' || next == ')') {
+                return null;
+            }
+            // will throw an error if not an integer
+            int end = i+1;
+            while (end < n && (data.charAt(end) == '-' || Character.isDigit(data.charAt(end)))) {
+                end++;
+            }
+            // throws if unexpected not a number
+            int val = Integer.parseInt(data, i, end, 10);
+
+            // advance the index and deserialize the subtrees
+            i = end;
+            TreeNode p = new TreeNode(val);
+            if (i < n && data.charAt(i) == '(') {
+                i++;
+                p.left = exec(data);
+                i++;
+                p.right = exec(data);
+                i++;
+            }
+            return p;
+        }
+    }
+
+    public static void main (String[] args) {
+        String s;
+        TreeNode node;
+
+        node = new TreeNode(1, new TreeNode(2), new TreeNode(3, new TreeNode(4), new TreeNode(5)));
+        s = new Codec().serialize(node);
+        System.out.println(s);
+
+        node = new Codec().deserialize(s);
+        System.out.println(node);
+
+        node = new TreeNode(1, null, new TreeNode(2));
+        s = new Codec().serialize(node);
+        System.out.println(s);
+
+        node = new Codec().deserialize(s);
+        System.out.println(node);
+    }
+
+    // Your Codec object will be instantiated and called as such:
+    // Codec ser = new Codec();
+    // Codec deser = new Codec();
+    // TreeNode ans = deser.deserialize(ser.serialize(root));
+}
