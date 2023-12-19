d98d6da11e1105e59b0fdf4a91dc3ebb05f3c4fa
Jeffrey Schang <schangj09@gmail.com>
2023-12-19 13:05:44 +0100

    buySellStock solution - 19 Dec 2023

---
+++ src/main/java/leetcode/buySellStock/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.buySellStock;
+
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+ */
+public class Solution {
+    public int maxProfit(int[] prices) {
+        // not dynamic programming - just need 2 variables - the min price seen before index i and the max profit
+        // found so far
+        int minPrice = prices[0];
+        int maxProfit = Integer.MIN_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            int p = prices[i];
+            if (p < minPrice) {
+                minPrice = p;
+            } else {
+                maxProfit = Math.max(maxProfit, p-minPrice);
+            }
+        }
+        return Math.max(maxProfit, 0);
+    }
+
+}
