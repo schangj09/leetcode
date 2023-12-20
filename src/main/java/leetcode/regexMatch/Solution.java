226886e4c71409e697b84b17713be5e4961cc49f
Jeffrey Schang <schangj09@gmail.com>
2023-12-20 06:38:34 +0100

    fix regex with recursion - 19 Dec 2023

--- src/main/java/leetcode/regexMatch/Solution.java
+++ src/main/java/leetcode/regexMatch/Solution.java
@@ -2,57 +2,46 @@ package leetcode.regexMatch;
 
 public class Solution {
     public boolean isMatch(String s, String p) {
-        // greedy consume as much as possible for * and backtrack as needed for a match
-
+        // use recursion
+        // the idea is to match the first character and then recurse on the rest of the
+        // string
         int n = s.length();
         int m = p.length();
-        int ip = 0;
-        int is = 0;
-        while (is < n && ip < m) {
-            char cs = s.charAt(is++);
-            char cp = p.charAt(ip++);
-            if (cp == '*') {
-                // bad expression
+        // note: a non-empty pattern can match an empty string, but an empty pattern only
+        // matches the empty string
+        if (m == 0) {
+            return n == m;
+        }
+        char cp = p.charAt(0);
+        boolean firstMatch = (n > 0 && (cp == '.' || cp == s.charAt(0)));
+        char lookahead = (m > 1) ? p.charAt(1) : 0;
+        // first char matches
+        // when it is a wildcard, then we need to recursively check either with an empty string
+        // and no wildcard or with one less char in the match string and keep the wildcard
+        if (lookahead == '*') {
+            return isMatch(s, p.substring(2))
+                    || (firstMatch && isMatch(s.substring(1), p));
+        } else {
+            if (firstMatch) {
+                // no wildcard and a single char match), so we can just match the rest of the
+                // pattern to the rest of the string
+                return isMatch(s.substring(1), p.substring(1));
+            } else {
+                // non matching char
                 return false;
             }
-            char lookahead = (ip < m) ? p.charAt(ip) : 0;
-            // check for wildacard match ".*" or "a*"
-            if (lookahead == '*') {
-                // increment ip for the lookahead char
-                ip++;
-                // if wildcard match, then go to end of string
-                // otherwise forward until cs is not equal to prev, then back up 
-                if (cp == '.') {
-                    is = n;
-                } else {
-                    char wildcardmatch = cp; // the character from the pattern we are looking for
-                    while (wildcardmatch == cs && is < n) {
-                        cs = s.charAt(is++);
-                    }
-                    // if cs doesn't match, then we went past it so back up one
-                    if (wildcardmatch != cs) {
-                        is--;
-                    }
-                }
-            } else {
-                // case: no wildcard 
-                // no match if characters don't match
-                if (cp != '.' && cp != cs) {
-                    return false;
-                }
-            }
         }
-        return is == n && ip == m;
     }
 
     public static void main(String[] args) {
         String[][] s = new String[][] {
-            // {"aa", "a"},
-            //{"aa", "a*"}
-            // {"ab", ".*"},
-            // {"auuiabx", ".*ab."},
-            // {"mississippi", "mis*is*p*."},
-            {"aab", "c*a*b"}
+                { "aa", "a" },
+                { "aa", "a*" },
+                { "ab", ".*" },
+                { "auuiabx", ".*ab." },
+                { "mississippi", "mis*is*p*." },
+                { "aab", "c*a*b" },
+                { "baaaaa", "baa*a" }
         };
 
         for (int i = 0; i < s.length; i++) {
