fad6daaa05c549f248bda1cdecf23397689c385c
Jeffrey Schang <schangj09@gmail.com>
2023-12-20 11:50:08 +0100

    optimal space solution from suggestions - 20 Dec 2023

--- src/main/java/leetcode/lowestCommonAncestor/Solution3.java
+++ src/main/java/leetcode/lowestCommonAncestor/Solution3.java
@@ -1,5 +1,5 @@
 package leetcode.lowestCommonAncestor;
-
+import java.util.*;
 
 public class Solution3 {
 
@@ -12,6 +12,40 @@ public class Solution3 {
     };
 
     public Node lowestCommonAncestor(Node p, Node q) {
+        // one approach
+        // can make a stack for going up parents of p, and a another stack for q
+        // then pop both stacks until we get a non-matching node
+
+        // is there a way to use less space?
+        // we don't need a stack, but really just a set of all parent nodes (including p)
+        // then find the first parent of q in the set
         
+        // alternative without extra space - recognize that we have 2 overlapping linked lists
+        // if their lengths are the same we can get to the matching node by iterating them 
+        // together
+        // to make the same length, can count length of each and reduce the longer one
+        int l1 = height(p);
+        int l2 = height(q);
+        while (l1 > l2) {
+            p = p.parent;
+            l1--;
+        }
+        while (l2 > l1) {
+            q = q.parent;
+            l2--;
+        }
+        while (p != q && p != null) {
+            p = p.parent;
+            q = q.parent;
+        }
+        return p;
+    }
+    int height(Node p) {
+        int c = 0;
+        while (p != null) {
+            c++;
+            p = p.parent;
+        }
+        return c
     }
 }
