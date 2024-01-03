50de73c7037315f0572c54201fd8c21ffb7cefe0
Jeffrey Schang <schangj09@gmail.com>
2024-01-03 13:52:37 +0100

    correct solution from the site - gave up

--- src/main/java/leetcode/basicCalculator2/Solution.java
+++ src/main/java/leetcode/basicCalculator2/Solution.java
@@ -17,84 +17,39 @@ s length up to 50k characters.
  */
 public class Solution {
     public int calculate(String s) {
-        // we want to calculate the result as we go and push the result on to a stack when precedence rules apply
-        // a) to simplify, treat subtraction as plus negative number
-        // b) if '-' follows another operator (or at beginning), then it must be a negative sign, else it is a subtraction
-        // ex: 3+2*-5 -> operator '*' is higher precedence than '+', so push operand and sign
-        // ex: 3*7/2-5
 
-        // a valid expression must always alternate between a number and operator
-        // concept is to keep track of previous operator, then get the next number and the lookeahead operator
-        // (always convert subtraction to a negative number)
-
-        // a + b*c
-        // if prevOp = +, if lookahead is same precedence, then add numbers, else push operand
-        // if prevOp = */, then always mult or divide, afterwards if lookahead is same precedence continue, else pop stack and add
-
-        Stack<Integer> stack = new Stack<>();
-        Stack<Character> opStack = new Stack<>();
-        int n = s.length();
-        int i = 0;
-        int operand = 0;
-        Character prevOp = null;
-        while (i < n) {
-            char c = s.charAt(i++);
-            // skip spaces
-            while (i < n && c == ' ') {
-                c = s.charAt(i++);
+        if (s == null || s.isEmpty()) return 0;
+        int len = s.length();
+        Stack<Integer> stack = new Stack<Integer>();
+        int currentNumber = 0;
+        char operation = '+';
+        for (int i = 0; i < len; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
             }
-            // parse the digit (handle possible leading negative sign)
-            int sign = 1;
-            if (c == '-') {
-                sign = -1;
-                c = s.charAt(i++);
-            }
-            int next = 0;
-            while (Character.isDigit(c)) {
-                next = next*10 + (c - '0');
-                c = i < n ? s.charAt(i++) : '+';
-            }
-            next *= sign;
-
-            // skip spaces after the number and before the next operator
-            while (i < n && s.charAt(i) == ' ') {
-                c = i < n ? s.charAt(i++) : '+';
-            }
-
-            if (prevOp == null) {
-                operand = next;
-            } else {
-                // if prevOp is mult/divide, then apply it
-                if (prevOp == '*' || prevOp == '/') {
-                    if (prevOp == '*') {
-                        operand = operand * next;
-                    } else {
-                        operand = operand / next;
-                    }
-                    // in case lookahead is lower precedence, we must pop the stack and add that value to the current operand
-                    if (!stack.isEmpty() && (c == '+' || c == '-')) {
-                        prevOp = opStack.pop();
-                        operand = stack.pop() + (prevOp == '-' ? -1 : 1) * operand;
-                    }
-                } else {
-                    // if lookeahead is higher precedence than prevOp, then push operand to the stack
-                    if (c == '*' || c == '/') {
-                        stack.push(operand);
-                        opStack.push(prevOp);
-                        operand = next;
-                    } else {
-                        // otherwise lookahead is same precedence as prevOp, so add next to operand
-                        operand = operand + (prevOp == '-' ? -1 : 1) * next;
-                    }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == len - 1) {
+                if (operation == '-') {
+                    stack.push(-currentNumber);
                 }
+                else if (operation == '+') {
+                    stack.push(currentNumber);
+                }
+                else if (operation == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                }
+                else if (operation == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                }
+                operation = currentChar;
+                currentNumber = 0;
             }
-            prevOp = c;
         }
-        // special case if anything is remaining on the stack add it
+        int result = 0;
         while (!stack.isEmpty()) {
-            prevOp = opStack.pop();
-            operand = stack.pop() + (prevOp == '-' ? -1 : 1) * operand;
+            result += stack.pop();
         }
-        return operand;
+        return result;
     }
+
 }
