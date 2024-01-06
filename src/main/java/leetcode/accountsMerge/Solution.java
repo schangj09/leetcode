025890b8bb9bdaad7f7783ba02f99e1cfec1cf5a
Jeffrey Schang <schangj09@gmail.com>
2024-01-06 13:09:29 +0100

    accounts merge - comment for email node based solution

--- src/main/java/leetcode/accountsMerge/Solution.java
+++ src/main/java/leetcode/accountsMerge/Solution.java
@@ -12,13 +12,29 @@ After merging the accounts, return the accounts in the following format: the fir
 
  */
 class Solution {
-    List<List<String>> result = new ArrayList<>();
+        List<List<String>> result = new ArrayList<>();
+
+    // Let N is number of accounts in input, K is max length of an account email
+    // Need at least O(NK) running time to go once through the input and O(NKlogNK) to build the result (beacuse of sorting).
+    // One idea is to make a graph of connected accounts - where node is the account and an edge exists if 2 accounts share an email
+    // Size of graph is N accounts, 2 edges for each account that has an overlap = O(N^2) in the worst case.
+    // Run time is iterate once on emails of every account in the input to build adjacency and once more to build output. 
+    // Plus sorting of merged account lists.
+    //
+    // Another idea is to make a graph of connected emails, where each node is an email and an edge exists.
+    // For 2 emails that belong to the same account. Not necessary to build all edges, just x-1 per account with x distinct emails.
+    // This guarantees that if 2 accounts overlap, then any shared node is connected to the others.
+    // Space is O(NK) for the adjacency graph (worst case every unique email is connected to every account)
+    // Time is O(NKlogNK) - once to build graph
+
+    // which algorithm is better depends on the nature of the input. 
+    // For a large number of accounts with few accounts to merge, it is more efficient to represent the 
 
     // idea is to make a graph and traverse connected accounts and merge
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         int n = accounts.size();
 
-        // the graph of connected account edges
+// the graph of connected account edges
         Map<Integer, Set<Integer>> g = new HashMap<>();
 
         // map of email to account id
@@ -46,7 +62,7 @@ class Solution {
         for (int k : g.keySet()) {
             if (!visited.contains(k)) {
                 Set<String> acctEmails = new HashSet<>();
-                dfs(g, k, visited, accounts, acctEmails);
+                dfs(k, visited, g, accounts, acctEmails);
 
                 List<String> emlList = new ArrayList<>(acctEmails);
                 emlList.sort(Comparator.naturalOrder());
@@ -57,7 +73,7 @@ class Solution {
         return result;
     }
 
-    void dfs(Map<Integer, Set<Integer>> g, int k, Set<Integer> visited, List<List<String>> accounts, Set<String> acctEmails) {
+    void dfs(int k, Set<Integer> visited, Map<Integer, Set<Integer>> g, List<List<String>> accounts, Set<String> acctEmails) {
         visited.add(k);
         List<String> kEml = accounts.get(k);
         for (int e = 1; e < kEml.size(); e++) {
@@ -65,8 +81,9 @@ class Solution {
         }
         for (int l : g.get(k)) {
             if (!visited.contains(l)) {
-                dfs(g, l, visited, accounts, acctEmails);
+                dfs(l, visited, g, accounts, acctEmails);
             }
         }
+
     }
 }
