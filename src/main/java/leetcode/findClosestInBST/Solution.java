6c7d87a2d3b1f7d7b490b7f1174311cad42eb3fe
Jeffrey Schang <schangj09@gmail.com>
2024-01-11 11:04:46 +0100

    find closest in BST solved - 11 Jan 2024

---
+++ src/main/java/leetcode/findClosestInBST/Solution.java
@@ -0,0 +1,51 @@
+package leetcode.findClosestInBST;
+
+/*
+Given the root of a binary search tree and a target value, return the value in the BST that is closest to the target. If there are multiple answers, print the smallest.
+
+
+*/
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int closestValue(TreeNode root, double target) {
+        return find(root, target, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    }
+
+    int find(TreeNode node, double target, int minAncestor, int maxAncestor) {
+        if (node == null) {
+            if (minAncestor == Integer.MAX_VALUE) {
+                return maxAncestor;
+            }
+            if (maxAncestor == Integer.MIN_VALUE) {
+                return minAncestor;
+            }
+            return (Math.abs(minAncestor - target) < Math.abs(maxAncestor - target))
+                    ? minAncestor
+                    : maxAncestor;
+        }
+        if ((double) node.val == target) {
+            return node.val;
+        }
+        if (node.val > target) {
+            minAncestor = Math.min(node.val, minAncestor);
+            return find(node.left, target, minAncestor, maxAncestor);
+        } else {
+            maxAncestor = Math.max(node.val, maxAncestor);
+            return find(node.right, target, minAncestor, maxAncestor);
+        }
+    }
+}
