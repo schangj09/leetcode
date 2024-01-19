e70dea390f0faddbe7d3cdff1be9f64eb933b94e
Jeffrey Schang <schangj09@gmail.com>
2024-01-19 18:34:27 +0100

    solved min dist shortest distance from buildings, split test cases into a separate file - 19 Jan 2024

--- src/main/java/leetcode/minTravelDist/Solution.java
+++ src/main/java/leetcode/minTravelDist/Solution.java
@@ -12,19 +12,21 @@ Hard
     each 1 marks a building that you cannot pass through, and
     each 2 marks an obstacle that you cannot pass through.
 
-You want to build a house on an empty land that reaches all buildings in the shortest total travel distance. You can only 
-move up, down, left, and right.
+You want to build a house on an empty land that reaches all buildings in the shortest total travel distance. 
+You can only move up, down, left, and right.
 
-Return the shortest travel distance for such a house. If it is not possible to build such a house according to the above 
-rules, return -1.
+Return the shortest travel distance for such a house. If it is not possible to build such a house according 
+to the above rules, return -1.
 
 The total travel distance is the sum of the distances between the houses of the friends and the meeting point.
  */
 public class Solution {
 
-    // Not solved - need to revisit this - possibly need some memoization
-    //
-    //
+    // The idea is to perform a BFS simultaneously starting from each building.
+    // For an empty cell, record the current dist to reach that cell from building i.
+    // Option: for k buildings, maintain an array of k values per grid cell
+    // After BFS, we find the cell with the minimum sum of k distances.
+    // Optimize - instead of storing each of k distances, we only need to
     public int shortestDistance(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -34,71 +36,54 @@ public class Solution {
         int numBuildings = buildings.size();
         // compute the travel distance from each building to each node i,j - it will be
         // 0 for non-reachable
+        int[][] adj = new int[][] {
+                // left, up, right, down
+                { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 }
+        };
         int[][][] travelDist = new int[m][n][numBuildings];
         for (int k = 0; k < numBuildings; k++) {
-            Integer[] building = buildings.get(k);
-            boolean[][] visited = new boolean[m][n];
 
-            // filling in the minimumm travelDist[][][k] value for every empty node in the
-            // grid
-            // computeDist(building, grid, visited, travelDist);
-
-            int[][] adj = new int[][] {
-                    // left, up, right, down
-                    { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 }
-            };
+            // BFS search to fill in the minimumm travelDist[][][k] value for every empty
+            // node in the grid
             Deque<Integer[]> q = new LinkedList<>();
-            q.offerLast(building);
+            q.offerLast(buildings.get(k));
+            int dist = 1;
             while (!q.isEmpty()) {
-                Integer[] node = q.pollFirst();
-                // mark as visited so we don't come back to it
-                visited[node[0]][node[1]] = true;
-                // only proceed to adjacents for empty nodes - skip this node if it is a
-                // building or an obstacle
-                if (grid[node[0]][node[1]] == 0 || (node == building)) {
-                    // for each adjacent node in the bounds, compute the minimum travel distance
-                    // from building k to that node
+                // process all the nodes at this level
+                for (int l = q.size(); l > 0; l--) {
+                    Integer[] node = q.pollFirst();
+
+                    // for each adjacent node in the bounds, set the travel distance
+                    // from building k to that node if it hasn't already been set
                     for (int e = 0; e < 4; e++) {
                         int u = node[0] + adj[e][0];
                         int v = node[1] + adj[e][1];
                         if (u >= 0 && u < m && v >= 0 && v < n) {
-                            // ok we have an adjacent node, if it is an empty lot, then the distance is
-                            // this node min dist + 1, unless it already has a lower minimum travel dist
-                            // System.out.println(String.format("%d, %d, %d, %d, %s", node[0], node[1], u,
-                            // v, travelDist[u][v][k]));
-                            if (grid[u][v] == 0) {
-                                int minDist = travelDist[node[0]][node[1]][k] + 1;
-                                if (travelDist[u][v][k] == 0) {
-                                    travelDist[u][v][k] = minDist;
-                                } else {
-                                    travelDist[u][v][k] = Math.min(travelDist[u][v][k], minDist);
-                                }
-                            }
-                            // System.out.println(String.format("%d, %d, %d, %d, %s", node[0], node[1], u,
-                            // v, travelDist[u][v][k]));
-                            // if adjacent node is not yet visited, then add it to the queue
-                            if (!visited[u][v]) {
+                            // if adjacent node is not yet set, then mark the distance and it to the queue
+                            if (grid[u][v] == 0 && travelDist[u][v][k] == 0) {
+                                travelDist[u][v][k] = dist;
                                 q.offerLast(new Integer[] { u, v });
                             }
                         }
                     }
                 }
+                dist++;
             }
         }
 
-        // compute the travel distance to buildings by summing the distance for all
-        // buildings
-        // exclude nodes that are not empty or from which all buildings are not
-        // reachable
+        // compute the travel distance to buildings by summing the distance for all buildings
+        // exclude nodes that are not empty or from which all buildings are not reachable
         int minsum = Integer.MAX_VALUE;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 0) {
+                    // System.out.println(String.format("%d %d %s", i, j,
+                    // Arrays.toString(travelDist[i][j])));
                     minsum = Math.min(minsum, sumdist(travelDist[i][j]));
                 }
             }
         }
-        return minsum < Integer.MAX_VALUE ? minsum : 0;
+        return minsum < Integer.MAX_VALUE ? minsum : -1;
     }
 
     List<Integer[]> findBuildings(int[][] grid) {
@@ -127,75 +112,15 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[][] g = new int[][] { { 1, 0, 2, 0, 1 }, { 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0 } };
+        int[][] g = TestCases.G1;
         System.out.println(new Solution().shortestDistance(g));
 
         // grid with unreachable buildings
-        g = new int[][] { { 1, 0, 2, 0, 1 }, { 0, 2, 0, 0, 0 }, { 2, 0, 1, 0, 0 } };
+        g = TestCases.G2;
         System.out.println(new Solution().shortestDistance(g));
 
-        g = new int[][]
-
-        { { 2, 2, 2, 2, 0, 2, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 1, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0,
-                0, 2, 2, 2, 0, 2, 0, 0 },
-                { 0, 0, 2, 2, 0, 0, 0, 2, 0, 0, 2, 2, 0, 0, 0, 0, 2, 0, 2, 1, 0, 2, 0, 1, 2, 2, 2, 0, 0, 2, 0, 2, 2, 0,
-                        0, 0, 0, 2, 2, 0, 2, 1, 2, 0 },
-                { 1, 0, 0, 0, 0, 2, 2, 0, 0, 2, 0, 0, 1, 0, 0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 2,
-                        0, 2, 2, 2, 0, 2, 0, 2, 2, 0 },
-                { 1, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0,
-                        0, 0, 2, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 1, 0, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0,
-                        2, 0, 2, 0, 1, 0, 2, 2, 0, 2 },
-                { 2, 0, 0, 0, 0, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 1, 2,
-                        0, 2, 2, 0, 0, 0, 2, 0, 0, 2 },
-                { 0, 0, 0, 2, 2, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0, 2, 0, 2, 0, 0, 2, 0, 2, 0, 1, 0, 2, 2, 0, 0, 2, 0, 0,
-                        0, 2, 2, 2, 2, 0, 0, 0, 2, 0 },
-                { 2, 0, 0, 2, 0, 2, 2, 2, 2, 0, 0, 2, 2, 0, 2, 0, 0, 0, 1, 2, 0, 2, 2, 0, 2, 0, 2, 1, 2, 0, 1, 0, 2, 1,
-                        2, 0, 0, 0, 0, 0, 0, 0, 2, 2 },
-                { 0, 0, 2, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 2, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2, 1, 2, 0, 0, 0, 2,
-                        1, 0, 2, 2, 2, 0, 0, 0, 2, 0 },
-                { 2, 0, 0, 0, 0, 2, 0, 0, 2, 2, 2, 2, 0, 2, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 1, 0, 0, 2, 2, 2,
-                        1, 2, 2, 0, 0, 0, 0, 0, 2, 2 },
-                { 0, 1, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 2, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 0, 2, 0, 2, 2, 2,
-                        0, 0, 2, 0, 0, 0, 0, 0, 2, 2 },
-                { 0, 2, 0, 0, 0, 2, 0, 2, 2, 2, 0, 0, 1, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0,
-                        0, 0, 0, 0, 0, 0, 2, 0, 0, 2 },
-                { 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 2, 0, 2, 2, 2, 0, 2, 0, 2, 0, 2, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 2, 2,
-                        2, 2, 0, 0, 0, 2, 0, 2, 2, 0 },
-                { 0, 0, 2, 0, 2, 2, 0, 0, 0, 0, 0, 0, 2, 2, 0, 2, 0, 2, 2, 0, 0, 0, 0, 0, 2, 2, 1, 0, 0, 1, 2, 0, 2, 0,
-                        0, 0, 0, 2, 0, 2, 0, 2, 0, 0 },
-                { 0, 2, 2, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 2, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0,
-                        2, 0, 0, 0, 0, 2, 0, 2, 0, 0 },
-                { 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 2, 2, 2, 0, 2, 2, 0, 2, 0, 2, 0, 2, 2, 0, 1, 0, 2, 0, 2, 0, 0,
-                        2, 0, 0, 0, 0, 0, 0, 2, 0, 2 },
-                { 2, 2, 0, 2, 0, 0, 1, 2, 0, 1, 0, 2, 0, 0, 2, 0, 1, 2, 1, 2, 2, 0, 2, 0, 0, 0, 0, 2, 2, 2, 0, 2, 0, 2,
-                        0, 2, 0, 0, 0, 0, 0, 0, 0, 2 },
-                { 0, 2, 2, 0, 2, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 2,
-                        0, 0, 0, 0, 0, 2, 2, 0, 2, 2 },
-                { 0, 2, 2, 0, 0, 2, 2, 0, 2, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 2, 2, 2, 1, 0, 2, 2, 0, 2, 0, 0, 2, 0, 0, 0,
-                        1, 2, 0, 2, 2, 0, 0, 0, 0, 0 },
-                { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 2, 2, 0, 0, 0, 2, 2, 1, 0, 0, 2, 2, 0, 2, 0, 2, 0,
-                        1, 0, 2, 0, 0, 0, 0, 2, 0, 2 },
-                { 0, 0, 2, 2, 0, 0, 2, 0, 2, 0, 2, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0,
-                        2, 0, 2, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 2, 2, 2, 2, 0, 0, 2, 0, 0, 2, 2, 0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 2, 0, 1, 2 },
-                { 2, 2, 2, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 2, 2, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 2, 0, 0, 0, 2, 2, 0,
-                        0, 2, 0, 0, 2, 0, 2, 2, 0, 2 },
-                { 2, 0, 0, 2, 2, 2, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 2, 0, 1, 0, 1, 0, 0, 2, 0,
-                        2, 0, 2, 0, 2, 1, 0, 2, 0, 0 },
-                { 2, 0, 0, 2, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 2, 2, 0, 0, 2, 0, 0, 2, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0,
-                        2, 0, 0, 2, 2, 1, 0, 0, 0, 2 },
-                { 1, 0, 2, 0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 1, 0, 0, 2, 2, 0, 0, 2, 0, 2, 0, 2, 2, 0, 0, 0, 2, 0, 0, 0, 0,
-                        2, 2, 2, 2, 0, 2, 2, 2, 0, 2 },
-                { 0, 0, 0, 0, 2, 2, 2, 0, 0, 1, 0, 2, 2, 2, 0, 0, 0, 1, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0,
-                        0, 0, 2, 0, 0, 1, 0, 0, 0, 2 },
-                { 0, 2, 2, 0, 2, 0, 2, 1, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 2, 2, 2, 0, 0, 2, 0, 2,
-                        0, 2, 0, 0, 0, 2, 0, 0, 0, 0 },
-                { 0, 2, 2, 0, 2, 2, 0, 2, 2, 0, 0, 0, 1, 2, 2, 0, 0, 2, 0, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 0, 0, 0, 2, 2,
-                        0, 0, 0, 0, 2, 2, 0, 2, 2, 2 },
-                { 0, 0, 2, 0, 0, 0, 1, 2, 0, 2, 2, 2, 0, 0, 2, 2, 0, 0, 0, 2, 0, 0, 2, 0, 0, 2, 2, 2, 0, 2, 2, 0, 0, 0,
-                        0, 2, 0, 0, 0, 0, 0, 0, 2, 2 } };
+        // large grid with unreachable buildings
+        g = TestCases.G3;
         System.out.println(new Solution().shortestDistance(g));
     }
 }
